2e9b9919e65607df5eaf940d7275770f362a7030
Artem Levitsky <shikamix@gmail.com>
2020-05-29 14:20:00 +0300

    Actual apps

---
+++ install_apps.ps1
@@ -0,0 +1,41 @@
+$apps = @(
+    "AgileBits.1Password"
+    "7zip.7zip"
+    "Discord.Discord"
+    "TimKosse.FilezillaClient"
+    "Git.Git"
+    "REALiX.HWiNFO"
+    "Apple.iTunes"
+    "LibreOffice.LibreOffice"
+    "Microsoft.Edge"
+    "Microsoft.PowerShell"
+    "Microsoft.VisualStudioCode"
+    "OpenJS.Nodejs"
+    "Notepad++.Notepad++"
+    "OBSProject.OBSStudio"
+    "Postman.Postman"
+    "Valve.Steam"
+    "Telegram.TelegramDesktop"
+    "Canonical.Ubuntu"
+    "VideoLAN.VLC"
+    "Microsoft.WindowsTerminal"
+)
+
+$wait_for_beta = @(
+    "Google.Chrome"
+)
+
+$wait_for_release = @(
+    "autohotkey"
+    "bitcomet"
+    "fork"
+    "totalcmd"
+    "unlocker"
+    "wamp"
+    "yandexdisk"
+    "workbench"
+)
+
+foreach ($app in $apps) {
+    winget install -e $app
+}
